4037639c5a642be8f997a418096a557c71f9a021
22178 <22178@192.168.76.1>
2020-04-04 03:20:35 +0000

    seconde commit

---
+++ springboot-hibernate-crud/src/main/java/nn/oksam90/springboothibernatecrud/SpringbootHibernateCrudExempleApplication.java
@@ -0,0 +1,13 @@
+package nn.oksam90.springboothibernatecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateCrudExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateCrudExempleApplication.class, args);
+	}
+
+}
---
+++ springboot-hibernate-crud/src/test/java/nn/oksam90/springboothibernatecrud/SpringbootHibernateCrudExempleApplicationTests.java
@@ -0,0 +1,13 @@
+package nn.oksam90.springboothibernatecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateCrudExempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
